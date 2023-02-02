db50abb43e0306d72e0a222df15a55c313fcb543
ADMIN <ADMIN@DESKTOP-RI9254C>
2023-02-02 15:22:33 +0700

    Add project files.

---
+++ Demo Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Bark();
+            dog.Eat();
+        }
+    }
+}
---
+++ RandomList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CustomRandomList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("Tan");
+            list.Add("Minh");
+            list.Add("Duy");
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
